1a8e09771c0e0efc7085363cacbd832b0868ffdb
Vinicius Fillos <vinifillos@gmail.com>
2024-10-25 09:46:56 -0300

    ci: migration for insert data into the database added

---
+++ src/main/resources/db/migration/V2__insert-into-foods.sql
@@ -0,0 +1,8 @@
+DELETE FROM foods;
+INSERT INTO foods (title, price, image)
+VALUES
+    ('Burger', 25, 'https://moinhoglobo.com.br/wp-content/uploads/2019/05/16-hamburguer.jpeg'),
+    ('Sushi', 45, 'https://th.bing.com/th/id/R.e525e62db4f88fea068b59da930e9d82?rik=05G5liPwuZsFXw&riu=http%3a%2f%2fculinarydestinations.net%2fwp-content%2fuploads%2f2015%2f07%2fjapan-sushi.jpeg&ehk=5KZEbUK%2bgFb5%2fr12OjFxxVssjimFy2%2fYnTE5vWABLwY%3d&risl=&pid=ImgRaw&r=0'),
+    ('Pizza', 35, 'https://www.comidaereceitas.com.br/wp-content/uploads/2007/12/pizza-calabresa-cr.jpg'),
+    ('Rice', 15, 'https://th.bing.com/th/id/R.5cf3826a3c0667b9f114b59a54cf27ce?rik=%2brUPj7T8JXXjzA&riu=http%3a%2f%2fnreceitas.com%2fwp-content%2fuploads%2f2022%2f12%2farroz-branco-scaled.jpg&ehk=EwjP6NAfwkEtODiwLlVpIBrXqCKZws6OqQ4JlYRPPPM%3d&risl=&pid=ImgRaw&r=0'),
+    ('Fries', 15, 'https://www.sabornamesa.com.br/media/k2/items/cache/377829b055e89e3afb894e7528a10996_XL.jpg');
